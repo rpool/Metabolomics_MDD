640bb7cea7d5e1b677b413e10a2a67097ac099f0
rpool <r.pool@vu.nl>
2024-09-19 10:06:15 +0200

    Added preprocessing of input and did a first testrun of MetWAS: seems to work.

--- Adjusted_MetWAS_lm.R
+++ Adjusted_MetWAS_lm.R
@@ -1,27 +1,67 @@
-#libraries
+# libraries
 library(tidyverse)
 
-metabolite_cidi_bmi <- read.table('Metabolomics_Aut2021/norm_metabolite_covariate_revisions.tsv', sep = '\t', header = TRUE)
+metabolite_mdd <- read.table(
+  "./Output/MDF.tsv",
+  sep = "\t",
+  header = TRUE
+)
+metabolite_mdd[, 66]
 
-#running the models 
-length = 249
-results <- data.frame(metabolite = 1:length, mdd_beta = 1:length, mdd_SE = 1:length, mdd_P = 1:length, n = 1:length)
+# running the models
+length <- 60
+results <- data.frame(
+  metabolite = 1:length,
+  mdd_beta = 1:length,
+  mdd_SE = 1:length,
+  mdd_P = 1:length,
+  n = 1:length
+)
 
-for(i in 3:251){
-  metabolite <- colnames(metabolite_cidi_bmi)[i]
+for (i in 6:65) {
+  metabolite <- colnames(metabolite_mdd)[i]
   print(i)
   print(metabolite)
-  mod <- lm(metabolite_cidi_bmi[,metabolite] ~ as.factor(Depressed.Ever) + scale(Age) + as.factor(Sex) + as.factor(AS) + scale(BMI) + as.factor(spectrometer) + as.factor(smoking_stat)+ as.factor(ethnicity_collapsed)+ as.factor(uni_nonuni) + scale(townsend_index) , data=metabolite_cidi_bmi)
-  results[i-1,1] <- metabolite
-  results[i-1,2] <- coef(summary(mod))[2]
-  results[i-1,3] <- coef(summary(mod))[39]
-  results[i-1,4] <- coef(summary(mod))[113]
-  results[i-1,5] <- nobs(mod)
+  mod <- lm(
+    metabolite_mdd[
+      ,
+      metabolite
+    ] ~
+      as.factor(MDD_status) + as.factor(batch_indicator),
+    data = metabolite_mdd
+  )
+  results[
+    i - 5,
+    1
+  ] <- metabolite
+  results[
+    i - 5,
+    2
+  ] <- coef(summary(mod))[2, 1]
+  results[
+    i - 5,
+    3
+  ] <- coef(summary(mod))[2, 2]
+  results[
+    i - 5,
+    4
+  ] <- coef(summary(mod))[2, 4]
+  results[
+    i - 5,
+    5
+  ] <- nobs(mod)
 }
 
+results$FDR_P <- p.adjust(
+  results[
+    ,
+    "mdd_P"
+  ],
+  method = "BH"
+)
+results <- results[order(results$FDR_P), ]
 
-results$FDR_P <- p.adjust(results[,"mdd_P"], method = 'BH')
-results <- results[order(results$FDR_P),]
-
-write.csv(results ,'Metabolomics_Aut2021/cidi_pheno/metabol_mdd_results/adjusted_results_all_covariates_revisions.csv', row.names = F) #change to appropriate file
-
+write.csv(results,
+  "./Output/Results_MDD.csv",
+  row.names = F
+) # change to appropriate file
